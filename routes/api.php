79a22a916f766763eb5dbed1cab12b849e40a1a7
Mortuja Morshed <75001023+Mortuja522@users.noreply.github.com>
2022-04-11 13:49:54 +0600

    mortuja Updated

--- routes/api.php
+++ routes/api.php
@@ -27,29 +27,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //=========Admin Start========//
 
-
- //------>user<----------
+//user Crud
 Route::get('/admin/userlist',[AdminAPIController::class,'userlist']);
 Route::post('/admin/add',[AdminAPIController::class,'adduser']);
 Route::post('/admin/edit/{id}',[AdminAPIController::class,'edituser']);
 Route::delete('/admin/delete/{id}',[AdminAPIController::class,'deleteuser']);
 Route::post('/admin/search',[AdminAPIController::class,'searchuser']);
-
-//------>admin profile<----------
-
+//admin profile
 Route::get('/admin/profile', [AdminAPIController::class, 'adminprofile']);
 Route::post('/admin/editProfile', [AdminAPIController::class, 'admineditProfile']);
-
- //------>Manager<----------
+//Manager Crud
 Route::get('/admin/managerlist',[AdminAPIController::class,'managerlist']);
 Route::post('/admin/addmanager',[AdminAPIController::class,'addmanager']);
 Route::post('/admin/editmanager/{id}',[AdminAPIController::class,'editmanager']);
 Route::delete('/admin/deletemanager/{id}',[AdminAPIController::class,'deletemanager']);
 Route::post('/admin/searchmanager',[AdminAPIController::class,'searchmanager']);
-
-
- //------>FlightManage<----------
-
+//flightmanager Crud
 Route::get('/admin/flightmanagerlist',[AdminAPIController::class,'flightmanagerlist']);
 Route::post('/admin/addflightmanager',[AdminAPIController::class,'addflightmanager']);
 Route::post('/admin/editflightmanager/{id}',[AdminAPIController::class,'editflightmanager']);
@@ -59,6 +52,8 @@ Route::post('/admin/searchflightmanager',[AdminAPIController::class,'searchfligh
 
 
 //=========Admin End=========//
+
+
 
 //=========Flight-Manager Start========//
 //Aircraft CRUD
